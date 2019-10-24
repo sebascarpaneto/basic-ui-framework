ddff4f7271b46c9c38ec0fbaeaa2119f4d13ca81
Sebastian Alejandro Carpaneto <sCarpaneto@truelogic.local>
2019-10-24 13:25:42 -0300

    Driver setup

---
+++ driver_setup/driver.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class Driver:
+    BROWSER = 'Chrome'
+    BASE_URL = 'http://www.aliexpress.com'
+
+    @staticmethod
+    def web_driver():
+        if Driver.BROWSER == 'Chrome':
+            driver = webdriver.Chrome(ChromeDriverManager().install())
+        else:
+            return 'Browser not supported'
+        driver.maximize_window()
+        driver.get(Driver.BASE_URL)
+        return driver
+
+    @staticmethod
+    def tear_down():
+        Driver.web_driver().quit()
