3e93d69862d8c356cf1099b5d7a251e2bec36bd4
Sebastian Alejandro Carpaneto <sCarpaneto@truelogic.local>
2019-12-18 17:44:09 -0300

    new_ui_core

--- ali_express_page_objects/second_page.py
+++
@@ -1,3 +0,0 @@
-from selenium.webdriver.common.by import By
-
-second_item = By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/li[2]/div/div[1]/a/img'
--- test/deviget_test.py
+++ test/deviget_test.py
@@ -1,31 +1,22 @@
 import pytest
 from assertpy import assert_that
 from driver_setup.driver import Driver
-from ali_express_page_objects.landing_page import close_pop_up, search_box, search, By
-from ali_express_page_objects.first_page import close_second_pop_up, next_page_button
-from ali_express_page_objects.second_page import second_item
-from ali_express_page_objects.item_page import quantity_element
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from ali_express_page_objects.landing_page import close_pop_up, search_box, search
 
-driver = Driver.web_driver()
-switch_tab = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.TAG_NAME, 'body'))).send_keys(
-    Keys.CONTROL + Keys.TAB)
+web_driver = Driver().driver()
 
 
-@pytest.mark.deviget
+@pytest.mark.test
 def ali_test():
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(close_pop_up)).click()
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(search_box)).send_keys('Iphone')
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(search)).click()
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(close_second_pop_up)).click()
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(next_page_button)).click()
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(second_item)).click()
-    switch_tab()
-    quantity = WebDriverWait(driver, 5).until(EC.element_to_be_clickable(quantity_element)).text()
-    assert_that(quantity, 1)
-    driver.quit()
+    web_driver.find_element(By.XPATH, '/html/body/div[5]/div/div/a').click()
+    web_driver.find_element(By.ID, 'search-key').click()
+    web_driver.find_element(By.XPATH, '//*[@id="form-searchbar"]/div[1]/input').send_keys('Iphone')
+    web_driver.quit()
 
 
 ali_test()
